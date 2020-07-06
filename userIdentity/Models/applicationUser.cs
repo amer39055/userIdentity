3e658dc8bb845b1fcb810d0aac002b94594d9ac4
amer k <aboessam84@hotmail.com>
2020-07-06 03:19:56 +0300

    application user added,

---
+++ userIdentity/Models/applicationUser.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+namespace userIdentity.Models
+{
+    public class applicationUser :IdentityUser
+    {
+        [Required]
+        [Display(Name ="username")]
+        [StringLength(30)]
+        public string Name { get; set; }
+
+        [DataType(DataType.DateTime)]
+        [Required]
+        public DateTime DateOfBirth { get; set; }
+
+        [Url]
+        public string url { get; set; }
+        
+        
+
+    }
+}
